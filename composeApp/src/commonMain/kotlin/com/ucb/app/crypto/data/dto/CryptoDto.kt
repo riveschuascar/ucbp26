e8c81c3ddc4e201f943a549a83de73aab3c1e618
unknown <huascar.rivero@ucb.edu.bo>
2026-03-25 19:57:57 -0400

    fix: agregar ? en dto para que si acepte valores nulos

--- composeApp/src/commonMain/kotlin/com/ucb/app/crypto/data/dto/CryptoDto.kt
+++ composeApp/src/commonMain/kotlin/com/ucb/app/crypto/data/dto/CryptoDto.kt
@@ -5,18 +5,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CryptoDto(
-    val id: String,
-    val name: String,
-    val symbol: String,
-    val image: String,
+    val id: String?,
+    val name: String?,
+    val symbol: String?,
+    val image: String?,
     @SerialName(value = "current_price")
-    val price: Double,
+    val price: Double?,
     @SerialName(value = "market_cap_rank")
-    val marketCapRank: Int,
+    val marketCapRank: Int?,
     @SerialName(value = "price_change_24h")
-    val priceChange24h: Double,
+    val priceChange24h: Double?,
     @SerialName(value = "high_24h")
-    val high24h: Double,
+    val high24h: Double?,
     @SerialName(value = "low_24h")
-    val low24h: Double
+    val low24h: Double?
 )
