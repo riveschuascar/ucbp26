4b2b74b05dd4b5954514a6a5a017aa38a8179ea3
unknown <huascar.rivero@ucb.edu.bo>
2026-03-25 19:25:45 -0400

    refact: agregar posibles valores nulos en ciertos campos

--- composeApp/src/commonMain/kotlin/com/ucb/app/crypto/data/mapper/CryptoMapper.kt
+++ composeApp/src/commonMain/kotlin/com/ucb/app/crypto/data/mapper/CryptoMapper.kt
@@ -4,13 +4,13 @@ import com.ucb.app.crypto.data.dto.CryptoDto
 import com.ucb.app.crypto.domain.model.CryptoModel
 
 fun CryptoDto.toModel() = CryptoModel(
-    id = id,
-    name = name,
-    symbol = symbol,
-    image = image,
-    price = price,
-    marketCapRank = marketCapRank,
-    priceChange24h = priceChange24h,
-    high24h = high24h,
-    low24h = low24h
+    id = id.orEmpty(),
+    name = name.orEmpty(),
+    symbol = symbol.orEmpty(),
+    image = image.orEmpty(),
+    price = price ?: 0.0,
+    marketCapRank = marketCapRank ?: 0,
+    priceChange24h = priceChange24h ?: 0.0,
+    high24h = high24h ?: 0.0,
+    low24h = low24h ?: 0.0
 )
